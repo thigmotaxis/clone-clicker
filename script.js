e428f3184a68cf0c95ae16684bdf19e6894915aa
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-06-13 13:20:42 -0700

    add function to generate cookiePanel with basic CSS

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-const body = document.querySelector("body")
-const cookie = document.createElement("img")
-cookie.setAttribute("src", "images/cookie.png")
-cookie.setAttribute("alt", "A large cookie. Maybe you should click it?")
-cookie.classList.add("bigCookie")
-body.appendChild(cookie)
+let cookieCount = 0;
+
+const htmlGenerator = (() => {
+  const body = document.querySelector("body")
+  const cookiePanel = () => {
+    const cookiePanelContainer = document.createElement("div")
+    cookiePanelContainer.classList.add("cookiePanel")
+    body.appendChild(cookiePanelContainer)
+
+    const bakeryName = document.createElement("div")
+    bakeryName.classList.add("bakeryName")
+    bakeryName.textContent = "Abe's Bakery"
+    cookiePanelContainer.appendChild(bakeryName)
+
+    const currentBank = document.createElement("div")
+    currentBank.classList.add("currentBank")
+    currentBank.textContent = cookieCount
+    cookiePanelContainer.appendChild(currentBank)
+
+    const cookie = document.createElement("img")
+    cookie.setAttribute("src", "images/cookie.png")
+    cookie.setAttribute("alt", "A large cookie. Maybe you should click it?")
+    cookie.classList.add("bigCookie")
+    cookiePanelContainer.appendChild(cookie)
+  }
+
+  return {cookiePanel}
+})()
+
+htmlGenerator.cookiePanel()
+
+
+
+
+// cookie.addEventListener("click", () => {
+//   let unoMas = incrementCookie(cookieCount)
+//   cookieCount = parseInt(unoMas)
+// })
+//
+// function incrementCookie(cookies) {
+//   cookies ++
+//   currentBank.textContent = cookies
+//   return cookies
+// }
